ea362a6c84ade9fe94ad6063d197cab752ad4fca
Max <108220707+Jolire@users.noreply.github.com>
2025-04-28 15:17:34 +0200

    6 my comment 5

--- src/test/java/com/cinema/filmlibrary/FilmLibraryApplicationTest.java
+++ src/test/java/com/cinema/filmlibrary/FilmLibraryApplicationTest.java
@@ -1,8 +1,15 @@
 package com.cinema.filmlibrary;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test; // Импортируем необходимую аннотацию для тестов
 
 @SpringBootTest
 class FilmLibraryApplicationTest {
-  // some code here
+
+    // Пример простого теста, который проверяет успешную загрузку контекста Spring
+    @Test
+    void contextLoads() {
+        // Этот тест проверяет, что контекст Spring успешно загружается
+        // Если контекст не загружается, тест завершится ошибкой
+    }
 }
